67393ff19b72ef3bb44064b71eb00e7fbba52ab2
gyuha <nicegyuha@gmail.com>
2022-11-03 15:51:15 +0900

    다국어 처리 추가

--- lang/ko_KR.ts
+++ lang/ko_KR.ts
@@ -4,9 +4,418 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../MainWindow.py" line="86"/>
+        <location filename="../app/ui/MainWindow.ui" line="20"/>
+        <source>Comics upscale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="88"/>
+        <source>Format</source>
+        <translation type="unfinished">포맷</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="96"/>
+        <source>jpg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="101"/>
+        <source>png</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="106"/>
+        <source>webp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="114"/>
+        <source>Upscale ratio</source>
+        <translation type="unfinished">업스케일 배율</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="122"/>
+        <location filename="../app/ui/MainWindow.ui" line="306"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="127"/>
+        <location filename="../app/ui/MainWindow.ui" line="311"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="132"/>
+        <location filename="../app/ui/MainWindow.ui" line="316"/>
+        <source>4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="140"/>
+        <source>Jpg optimize</source>
+        <translation type="unfinished">JPG 최적화</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="148"/>
+        <source>100 (OFF)</source>
+        <translation type="unfinished">100 (끄기)</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="153"/>
+        <source>99</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="158"/>
+        <source>98</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="163"/>
+        <source>97</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="168"/>
+        <source>96</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="173"/>
+        <source>95</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="178"/>
+        <source>94</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="183"/>
+        <source>93</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="188"/>
+        <source>92</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="193"/>
+        <source>91</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="198"/>
+        <source>90</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="203"/>
+        <source>89</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="208"/>
+        <source>88</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="213"/>
+        <source>87</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="218"/>
+        <source>86</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="223"/>
+        <source>85</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="228"/>
+        <source>84</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="233"/>
+        <source>83</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="238"/>
+        <source>82</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="243"/>
+        <source>81</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="248"/>
+        <source>80</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="253"/>
+        <source>79</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="258"/>
+        <source>78</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="263"/>
+        <source>77</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="268"/>
+        <source>76</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="273"/>
+        <source>75</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="288"/>
+        <source>Tile size</source>
+        <translation type="unfinished">타일 크기</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="296"/>
+        <source>0 (Auto)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="301"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="321"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="326"/>
+        <source>6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="331"/>
+        <source>7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="336"/>
+        <source>8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="341"/>
+        <source>9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="346"/>
+        <source>10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="351"/>
+        <source>11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="356"/>
+        <source>12</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="361"/>
+        <source>13</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="366"/>
+        <source>14</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="371"/>
+        <source>15</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="376"/>
+        <source>16</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="381"/>
+        <source>17</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="386"/>
+        <source>18</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="391"/>
+        <source>19</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="396"/>
+        <source>20</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="401"/>
+        <source>21</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="406"/>
+        <source>22</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="411"/>
+        <source>23</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="416"/>
+        <source>24</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="421"/>
+        <source>25</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="426"/>
+        <source>26</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="431"/>
+        <source>27</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="436"/>
+        <source>28</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="441"/>
+        <source>29</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="446"/>
+        <source>30</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="451"/>
+        <source>31</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="456"/>
+        <source>32</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="465"/>
+        <source>realesr-animevideov3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="470"/>
+        <source>realesrgan-x4plus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="475"/>
+        <source>realesrgan-x4plus-anime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="483"/>
+        <source>Model name</source>
+        <translation type="unfinished">모델</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="506"/>
+        <source>TTA Mode</source>
+        <translation type="unfinished">TTA 모드</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="513"/>
+        <source>TTA mode</source>
+        <translation type="unfinished">TTA 모드</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="520"/>
+        <location filename="../app/ui/MainWindow.ui" line="527"/>
+        <source>Replace origin</source>
+        <translation type="unfinished">원본 대체</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="568"/>
+        <source>Add folder</source>
+        <translation type="unfinished">폴더 추가</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="575"/>
+        <source>Add file</source>
+        <translation type="unfinished">파일 추가</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="595"/>
+        <source>List clear</source>
+        <translation type="unfinished">목록 지우기</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="602"/>
+        <source>Done Clear</source>
+        <translation type="unfinished">완료 지우기</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="609"/>
         <source>Start</source>
         <translation type="unfinished">시작</translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="625"/>
+        <source>항상위</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="636"/>
+        <source>클립보드에서 추가</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/ui/MainWindow.ui" line="641"/>
+        <source>종료(&amp;q)</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
